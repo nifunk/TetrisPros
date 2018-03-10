bb9670f0c1390cda30e9f7601ae797315694e30a
Simon Schaefer <s-h-schaefer@web.de>
2018-03-10 17:48:07 +0800

    changed results to double reward and state array

--- game/Results.java
+++ game/Results.java
@@ -2,15 +2,11 @@ package game;
 
 public class Results {
 
-    public int reward;
-    public int state;
+    public double reward;
+    public int[] state;
     public boolean terminated;
 
-    public Results(final int rew, final int sta, final boolean term) {
+    public Results(final int rew, final int[] sta, final boolean term) {
         reward = rew; state = sta; terminated = term;
-    }
-
-    public Results() {
-        reward = 0; state = 0; terminated = false;
     }
 }
